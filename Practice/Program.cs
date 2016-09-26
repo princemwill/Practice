5de885464a10c255cb0aecbf0dbc368104e397fc
Prince <princemwilliams@gmail.com>
2016-09-26 09:10:23 -0400

    First. Commit, just playing around with practice code challenges from Mel.

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -10,6 +10,16 @@ namespace Practice
     {
         static void Main(string[] args)
         {
+
+            string[] clock = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+
+            for (int i = 0; i < 24; i++)
+            {
+                Console.WriteLine(clock[i % clock.Length]);
+
+                
+            }
+            Console.ReadKey();
         }
     }
 }
